ea16595c51fc0d8bc1b5258d985b0c8a20d23ba9
Felipe Augusto Silva <felipeas@ciant.com>
2022-03-03 20:31:56 -0300

    starter files

---
+++ Restaurant/RestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantApp.swift
+//  Restaurant
+//
+//  Created by FELIPE AUGUSTO SILVA on 03/03/22.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
