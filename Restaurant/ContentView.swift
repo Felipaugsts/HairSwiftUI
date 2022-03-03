15953160a03e1ce506a9dee52c62c9f2c519d5df
Felipe Augusto Silva <felipeas@ciant.com>
2022-03-03 20:46:42 -0300

    feat: navigation bar ui

--- Restaurant/ContentView.swift
+++ Restaurant/ContentView.swift
@@ -9,10 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack (alignment: .bottom) {
+            VStack (alignment: .leading) {
+                HStack {
+                    Image(systemName: "line.horizontal.3")
+                        .resizable()
+                        .frame(width: 24, height: 24)
+                    Spacer()
+                    
+                    Image(systemName: "magnifyingglass")
+                        .resizable()
+                        .frame(width: 20, height: 20)
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(.secondary)
+                        .clipShape(Circle())
+                        .shadow(radius: 8)
+                }
+                    
+
+                
+            }
+        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
