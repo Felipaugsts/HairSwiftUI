621b20838cb1806b10438f90ea55b2dbddb99839
Felipe Augusto Silva <felipeas@ciant.com>
2022-03-03 21:13:52 -0300

    feat: card items

--- Restaurant/ContentView.swift
+++ Restaurant/ContentView.swift
@@ -26,14 +26,83 @@ struct ContentView: View {
                         .clipShape(Circle())
                         .shadow(radius: 8)
                 }
-                    
-
-                
+                    Text("Food Bazaar")
+                        .font(.title)
+                        .foregroundColor(.secondary)
+                    Text("26 results")
+                    .font(.subheadline)
+                        .foregroundColor(.primary)
+                        
+                VStack(alignment: .leading) {
+                    Text("Classic")
+                        .font(.title)
+                        .padding(.top, 10)
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack(spacing: 20) {
+                            FoodItem()
+                            FoodItem()
+                            FoodItem()
+                            FoodItem()
+                            FoodItem()
+                            
+                        }
+                      
+                    }
+                }
+                Spacer()
+                }
+            .padding()
+            .edgesIgnoringSafeArea(.bottom)
+        Button(action: {}) {
+            Image(systemName: "cart")
+                .resizable()
+                .frame(width: 12, height: 12)
+                .padding()
+                .background(.yellow)
+                .clipShape(Circle())
+                .padding()
+            Text("4 items")
+                .foregroundColor(.white)
+                .padding(.horizontal)
             }
+                .background(.black.opacity(0.8))
+                .clipShape(Capsule())
+                .shadow(color: .black.opacity(0.2), radius: 20, x: 0, y: 20)
         }
     }
 }
 
+struct FoodItem: View {
+    var body: some View {
+        VStack (alignment: .leading) {
+            HStack {
+                Image("flame.fill")
+                    .resizable()
+                    .frame(width: 16, height: 16)
+                    .foregroundColor(.red)
+                Text("Very hot")
+                    .font(.footnote)
+                    .foregroundColor(.primary)
+            }
+            Image("food")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 100, height: 100)
+                .shadow(radius: 8)
+            Text("Classic")
+                .font(.headline)
+            Text("Platter")
+                .foregroundColor(.secondary)
+                .font(.subheadline)
+                .padding(.bottom, 12)
+            Text("$12.99")
+                .font(.title)
+        }.frame(width: 180, height: 250)
+            .background(Color.secondary.opacity(0.3))
+            .cornerRadius(12)
+            
+    }
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
