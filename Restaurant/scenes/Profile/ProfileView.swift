c788f216f2819b86abe1b6f317dbeab722840f0a
Felipe Augusto Silva <felipeas@ciant.com>
2022-03-16 16:48:13 -0300

    feat: adding profile view

--- Restaurant/scenes/Profile/ProfileView.swift
+++ Restaurant/scenes/Profile/ProfileView.swift
@@ -8,8 +8,42 @@
 import SwiftUI
 
 struct ProfileView: View {
+    @State var name = "Felipe Augusto"
+    @State var city = "Moncton, NB"
+    @State var email = "Email"
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Image("hairlogo")
+                .resizable()
+                .frame(width: 150, height: 150)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(Color.white, lineWidth: 4))
+                .shadow(radius: 2)
+            
+            ULineTextField(fieldText: "Username", field: $name)
+                .padding([.top, .leading, .trailing], 30)
+            ULineTextField(fieldText: "City", field: $city)
+                .padding([.top, .leading, .trailing], 30)
+            ULineTextField(fieldText: "Email", field: $email)
+                .padding([.top, .leading, .trailing], 30)
+            HStack {
+                Spacer()
+                Button(action: {}) {
+                    Text("Save")
+                        .padding()
+                        .foregroundColor(.white)
+                        .font(.body)
+                        
+                }
+                .frame(width: 110, height: 40)
+                .background(.blue.opacity(0.7))
+                .cornerRadius(5)
+                
+            }
+            .padding(.trailing, 43)
+            Spacer()
+        }
+        .frame(width: UIScreen.main.bounds.width, height: 500)
     }
 }
 
